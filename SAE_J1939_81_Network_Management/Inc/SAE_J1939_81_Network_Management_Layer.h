147f75e25a02a7089af8baafe99f9d01fa478d3a
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-05-05 19:16:02 +0200

    [NL] feat: add empty files

---
+++ SAE_J1939_81_Network_Management/Inc/SAE_J1939_81_Network_Management_Layer.h
@@ -0,0 +1,23 @@
+/**
+  ******************************************************************************
+  * @file    SAE_J1939_81_Network_Management_Layer.h
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    05 May 2023
+  * @brief   Header file of SAE J1939-21 Network Management Layer.
+  *
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Define to prevent recursive inclusion
+//---------------------------------------------------------------------------
+#ifndef __SAE_J1939_21_NETWORK_MANAGEMENT_LAYER_H
+#define __SAE_J1939_21_NETWORK_MANAGEMENT_LAYER_H
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "stm32f4xx.h"
+
+#endif /* __SAE_J1939_21_NETWORK_MANAGEMENT_LAYER_H */
---
+++ SAE_J1939_81_Network_Management/Src/SAE_J1939_81_Network_Management_Layer.c
@@ -0,0 +1,16 @@
+/**
+  ******************************************************************************
+  * @file    SAE_J1939_81_Network_Management_Layer.h
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    05 May 2023
+  * @brief	 This file contains the implementation of functions for a network
+  * 		 management layer of SAE J1939
+  *
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "SAE_J1939_81_Network_Management_Layer.h"

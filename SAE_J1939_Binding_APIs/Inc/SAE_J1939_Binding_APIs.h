dfd39700dfadcf718d32d016e4e3bb06c9ff378c
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-05-05 18:28:38 +0200

    [Bin_APIs] feat: add empty files

---
+++ SAE_J1939_Binding_APIs/Inc/SAE_J1939_Binding_APIs.h
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file    SAE_J1939_Binding_APIs.h
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    03 May 2023
+  * @brief   Header file of Binding APIs of SAE J1939.
+  *
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Define to prevent recursive inclusion
+//---------------------------------------------------------------------------
+#ifndef __SAE_J1939_BINDING_APIS_H
+#define __SAE_J1939_BINDING_APIS_H
+
+#endif /* __SAE_J1939_BINDING_APIS_H */

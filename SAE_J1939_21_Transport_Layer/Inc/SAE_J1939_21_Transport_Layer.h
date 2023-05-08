94bab5acab3126a92a5140e28f3c8b706249f018
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-05-08 18:50:03 +0200

    [TP.h] feat: add J1939_TP_CM and J1939_TP_DT enums

--- SAE_J1939_21_Transport_Layer/Inc/SAE_J1939_21_Transport_Layer.h
+++ SAE_J1939_21_Transport_Layer/Inc/SAE_J1939_21_Transport_Layer.h
@@ -25,8 +25,27 @@
 //---------------------------------------------------------------------------
 
 /**
- * @brief J1939 control bytes enumeration.
+ * @brief PGN 0x00EC00 - Transport Protocol Connection Management.
  */
+typedef struct
+{
+	uint8_t control_byte;							/* Type of messages	*/
+	uint16_t message_size;							/* Total bytes of sending message - 9 to 1785 */
+	uint8_t total_number_of_packages;				/* Number of packages to send a message */
+	uint32_t PGN_of_the_multipacket_message;		/* A PGN that activated the multi-packet transfer */
+} J1939_TP_CM;
+
+/**
+ * @brief PGN 0x00EB00 - Transport Protocol Data Transfer.
+ */
+typedef struct
+{
+	uint8_t sequence_number;		/* Sequence number - 1 to 255 */
+	uint8_t* data;					/* A pointer to data */
+	uint16_t data_size;				/* From 9 to MAX_DT_SIZE */
+	uint8_t flag_wait_message;		/* A flag of waiting message */
+} J1939_TP_DT;
+
 typedef enum
 {
 	CB_TP_CM_RTS 				= 16U,
